89929cd39d896d6cc8042de13e76609d87be5f0a
Andrew Yan <ayan@usgs.gov>
2017-07-21 11:01:14 -0500

    add post method

--- greetings/hello_world.py
+++ greetings/hello_world.py
@@ -13,3 +13,17 @@ class HelloWorld(object):
         resp_body = ujson.dumps(doc, ensure_ascii=False)
         resp.body = resp_body
         resp.status = falcon.HTTP_200
+
+    def on_post(self, req, resp):
+        payload = req.stream.read()
+        try:
+            parsed = ujson.loads(payload)
+        except ValueError:
+            resp.status = falcon.HTTP_415
+            resp.body = 'Only JSON content is supported.'
+        else:
+            keys = parsed.keys()
+            doc = {'payloadKeys': keys}
+            resp_body = ujson.dumps(doc, ensure_ascii=False)
+            resp.status = falcon.HTTP_201
+            resp.body = resp_body
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import falcon
 from falcon import testing
 import pytest
+import ujson
 
 from greetings.app import api
 
@@ -13,8 +14,20 @@ def client():
 
 # pytest will inject the object returned by the "client" function
 # as an additional parameter.
-def test_hello_world(client):
+def test_hello_world_get(client):
     response = client.simulate_get('/hello_world')
     assert response.status == falcon.HTTP_200
     assert "hello world" in response.json['message'].lower()
     assert "utc" in response.json['message'].lower()
+
+
+def test_hello_world_invalid_post(client):
+    response = client.simulate_post('/hello_world', body='<a>b</a>')
+    assert response.status == falcon.HTTP_415
+
+
+def test_hello_world_valid_post(client):
+    test_payload = ujson.dumps({'fruit': 'apple', 'color': 'red'})
+    response = client.simulate_post('/hello_world', body=test_payload)
+    assert response.status == falcon.HTTP_201
+    assert len(response.json['payloadKeys']) == 2

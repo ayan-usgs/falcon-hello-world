0ccc454c67ad882d55431bbaec7bbaeb7f409b3a
Andrew Yan <ayan@usgs.gov>
2017-08-25 11:41:44 -0500

    update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,8 @@
-bind = '0.0.0.0:7010'
+import os
+
+bind_ip = os.getenv('bind_ip', '0.0.0.0')
+bind_port = os.getenv('bind_port', '7010')
+bind = '{0}:{1}'.format(bind_ip, bind_port)
 capture_output = True
 keyfile = None
 certfile = None

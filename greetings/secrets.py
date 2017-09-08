cd7c19089548af98af49264d12681ac8a95ad15a
Andrew Yan <ayan@usgs.gov>
2017-09-08 14:53:46 -0500

    update secret handling

--- greetings/secrets.py
+++ greetings/secrets.py
@@ -1,3 +1,5 @@
+from hashlib import md5
+
 import falcon
 import ujson
 
@@ -10,7 +12,8 @@ class SecretText(object):
 
     def on_get(self, req, resp):
         app_logger.debug(self.log_msg.format(req.method, req.uri))
-        doc = {'secretText': secret_text}
+        hash_result = md5(secret_text.encode('utf-8'))
+        doc = {'secretTextHash': hash_result.hexdigest()}
         resp_body = ujson.dumps(doc, ensure_ascii=False)
         resp.body = resp_body
         resp.status = falcon.HTTP_200

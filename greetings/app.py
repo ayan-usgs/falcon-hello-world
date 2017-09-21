42814e2957948cafbd8637ce6bdbb9bcb1cceec4
Andrew Yan <ayan@usgs.gov>
2017-09-21 08:34:08 -0500

    add useless swagger ui

--- greetings/app.py
+++ greetings/app.py
@@ -1,8 +1,13 @@
 import falcon
+from  falcon_swagger_ui import register_swaggerui_app
 
 from .hello_world import HelloWorld
 from .version import AppVersion
 from .secrets import SecretText
+
+
+SWAGGER_URL = '/swagger'
+API_URL = '/swagger.json'
 
 
 api = application = falcon.API()
@@ -14,3 +19,5 @@ st = SecretText()
 api.add_route('/hello_world', hi)
 api.add_route('/version', av)
 api.add_route('/secret', st)
+
+register_swaggerui_app(application, SWAGGER_URL, API_URL)

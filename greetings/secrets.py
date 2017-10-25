82f3345b37df189d9e2b9554e0de0ce2db33d1be
Andrew Yan <ayan@usgs.gov>
2017-10-25 11:29:44 -0500

    update json key

--- greetings/secrets.py
+++ greetings/secrets.py
@@ -13,7 +13,7 @@ class SecretText(object):
     def on_get(self, req, resp):
         app_logger.debug(self.log_msg.format(req.method, req.uri))
         hash_result = md5(secret_text.encode('utf-8'))
-        doc = {'secretTextHash': hash_result.hexdigest()}
+        doc = {'secretTextMD5Hash': hash_result.hexdigest()}
         resp_body = ujson.dumps(doc, ensure_ascii=False)
         resp.body = resp_body
         resp.status = falcon.HTTP_200
